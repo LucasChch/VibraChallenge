2be61e654b445d165065b34d0dcc608061c5ed69
LucasChCh <yolucasch99@gmail.com>
2024-03-19 19:01:29 -0300

    App.py con endpoint para buscar por querystring datos de un archivo .csv y devolverlos por websocket

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
 from flask import Flask, request, jsonify
 from flask_socketio import SocketIO
-import asyncio
 import csv
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return '<h1>Test</h1>'
+async def search_async(filename, name=None, city=None, quantity=None):
+    results = []
+
+    # Definir los nombres de los campos manualmente ya que el archivo CSV no tiene una fila de encabezado
+    fieldnames = ['id', 'name', 'surname', 'email', 'gender', 'company', 'city']
+
+    with open(filename, newline='', encoding='utf8') as csvfile:
+        reader = csv.DictReader(csvfile,fieldnames=fieldnames)
+        for index, row in enumerate(reader,1):
+            if (not name or row['name'] == name) and \
+               (not city or row['city'] == city):
+                results.append(row)
+                if quantity == index:
+                    break
+
+    return results
+
+#Endpoint requerido para la búsqueda en el archivo .csv
+@app.route('/search', methods=['GET'])
+async def search():
+    #Búsco los parámentros recibidos del querystring
+    name = request.args.get('name')
+    city = request.args.get('city')
+    quantity = request.args.get('quantity', type=int)
+
+    # Iniciar la búsqueda asincrónica
+    results =  await search_async('vibra_challenge.csv',name, city, quantity)
+
+    # Emitir resultados a través de WebSockets
+    socketio.emit('search_results', {'status': 'completed', 'results': results})
+
+    #Esto lo que veo en mi salida del endpoint
+    return jsonify({'status': 'Results delivered by websocket'})
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
