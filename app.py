8f17bc91f135c14c9f053b8326d3a8e9c78063fd
LucasChCh <yolucasch99@gmail.com>
2024-03-19 19:07:43 -0300

    Agrego try catch para control de errores

--- app.py
+++ app.py
@@ -11,33 +11,42 @@ async def search_async(filename, name=None, city=None, quantity=None):
     # Definir los nombres de los campos manualmente ya que el archivo CSV no tiene una fila de encabezado
     fieldnames = ['id', 'name', 'surname', 'email', 'gender', 'company', 'city']
 
-    with open(filename, newline='', encoding='utf8') as csvfile:
-        reader = csv.DictReader(csvfile,fieldnames=fieldnames)
-        for index, row in enumerate(reader,1):
-            if (not name or row['name'] == name) and \
-               (not city or row['city'] == city):
-                results.append(row)
-                if quantity == index:
-                    break
+    try:
+        with open(filename, newline='', encoding='utf8') as csvfile:
+            reader = csv.DictReader(csvfile,fieldnames=fieldnames)
+            for index, row in enumerate(reader,1):
+                if (not name or row['name'] == name) and \
+                (not city or row['city'] == city):
+                    results.append(row)
+                    if quantity == index:
+                        break
+    except Exception as e:
+        return(f"Error occurred: {e}")
 
     return results
 
 #Endpoint requerido para la búsqueda en el archivo .csv
 @app.route('/search', methods=['GET'])
 async def search():
-    #Búsco los parámentros recibidos del querystring
-    name = request.args.get('name')
-    city = request.args.get('city')
-    quantity = request.args.get('quantity', type=int)
+    try:
+        #Busco los parámentros recibidos del querystring
+        name = request.args.get('name')
+        city = request.args.get('city')
+        quantity = request.args.get('quantity', type=int)
 
-    # Iniciar la búsqueda asincrónica
-    results =  await search_async('vibra_challenge.csv',name, city, quantity)
+        # Iniciar la búsqueda asincrónica
+        results =  await search_async('vibra_challenge.csv',name, city, quantity)
 
-    # Emitir resultados a través de WebSockets
-    socketio.emit('search_results', {'status': 'completed', 'results': results})
+        # Emitir resultados a través de WebSockets
+        socketio.emit('search_results', {'status': 'completed', 'results': results})
 
-    #Esto lo que veo en mi salida del endpoint
-    return jsonify({'status': 'Results delivered by websocket'})
+        #Esto lo que veo en mi salida del endpoint
+        return jsonify({'status': 'Results delivered by websocket'})
+    
+    except Exception as e:
+        # Manejar la excepción aquí (puedes imprimir un mensaje de error, registrar el error, etc.)
+        print(f"Error occurred: {e}")
+        return jsonify({'status': 'Error occurred during search'})
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
